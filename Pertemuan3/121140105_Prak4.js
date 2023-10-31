9ede43fe55491c276f7350e2569505dfe328573f
JackReacher <91040803+ahmadrizki007@users.noreply.github.com>
2023-10-31 18:34:21 +0700

    Add files via upload

---
+++ Pertemuan3/121140105_Prak4.js
@@ -0,0 +1,17 @@
+function hitungGanjilGenap(bilangan) {
+    var jumlahGanjil = 0;
+    var jumlahGenap = 0;
+
+    for (var i = 1; i <= bilangan; i++) {
+        if (i % 2 === 0) {
+            jumlahGenap++;
+        } else {
+            jumlahGanjil++;
+        }
+    }
+;
+    console.log("Jumlah Ganjil: " + jumlahGanjil);
+    console.log("Jumlah Genap: " + jumlahGenap);
+}
+
+hitungGanjilGenap(50);
